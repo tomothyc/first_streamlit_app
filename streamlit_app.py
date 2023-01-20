45b2887bd73f6cb79ac542d634d24ede21c37f35
tomothyc <68897511+tomothyc@users.noreply.github.com>
2023-01-20 15:37:53 +0800

    Update streamlit_app.py
    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 
 streamlit.title("It's over Anakin, I have the high ground")
+streamlit.header("Your crush likes you back")
+streamlit.text("and other hilarious jokes you can tell yourself")
