d1474273d8b66edc632a4ffc79ee70aae1140010
tomothyc <68897511+tomothyc@users.noreply.github.com>
2023-01-20 15:44:23 +0800

    Update streamlit_app.py
    Added multiselect list

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,8 @@ streamlit.title("It's over Anakin, I have the high ground")
 streamlit.header("🥭 Your crush likes you back")
 streamlit.text("and other hilarious jokes you can tell yourself")
 
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick me:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
