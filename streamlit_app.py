b553b51c80d44d8ef2633cdb7273af4627387afa
tomothyc <68897511+tomothyc@users.noreply.github.com>
2023-01-20 15:42:57 +0800

    Update streamlit_app.py
    Added pandas functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("It's over Anakin, I have the high ground")
 streamlit.header("🥭 Your crush likes you back")
 streamlit.text("and other hilarious jokes you can tell yourself")
+
+streamlit.dataframe(my_fruit_list)
