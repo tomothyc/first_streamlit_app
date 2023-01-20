836da73752e0a47c9c69745b6825a728feb2dd42
tomothyc <68897511+tomothyc@users.noreply.github.com>
2023-01-20 15:47:37 +0800

    Update streamlit_app.py
    Moved default to correct location

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt", ['Banana', 'Peach'])
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title("It's over Anakin, I have the high ground")
@@ -9,7 +9,7 @@ streamlit.header("🥭 Your crush likes you back")
 streamlit.text("and other hilarious jokes you can tell yourself")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick me:", list(my_fruit_list.index))
+streamlit.multiselect("Pick me:", list(my_fruit_list.index), ['Banana', 'Peach'])
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
